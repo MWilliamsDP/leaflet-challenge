065eddbdcae5b9bdc51d8a0edb1a497848712024
mwilliamsDP <meganhwilliams@gmail.com>
2021-10-01 19:13:15 -0400

    final

--- static/js/logic1.js
+++ static/js/logic1.js
@@ -8,10 +8,10 @@ d3.json(queryUrl).then(function (data) {
   createMap(data.features);
 });
 
-// define the createMap function
+// define the createMap function and parameters
 function createMap(response) {
 
-    // Initial parameters to create map
+    
     var centerCoordinates = [39, -105];
     var mapZoom = 5;
 
@@ -21,7 +21,7 @@ function createMap(response) {
         zoom: mapZoom
     });
 
-    // Create the tile layer for map
+    // Create the tile layer 
     L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
         attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
         tileSize: 512,
@@ -34,7 +34,7 @@ function createMap(response) {
     // Create a GeoJSON layer 
     L.geoJSON(response, {
 
-        // use pointToLayer to create circle markers for each data's coordinates
+        // Create circle markers at locations
         pointToLayer: function(feature, latlng) {
             return L.circleMarker(latlng, {
                 radius: markerSize(feature.properties.mag),
@@ -46,17 +46,17 @@ function createMap(response) {
             });
         },
 
-        // Run the onEachFeature function once for each piece of data in the array
+        // Run the onEachFeature function 
         onEachFeature: onEachFeature
     }).addTo(myMap)
 
     // Binding a pop-up to each layer
     function onEachFeature(feature, layer) {
 
-        // date formatter for popup
+        
         var format = d3.timeFormat("%d-%b-%Y at %H:%M");
 
-        layer.bindPopup(`<strong>Place: </strong> ${feature.properties.place}<br><strong>Time: </strong>${format(new Date(feature.properties.time))}<br><strong>Magnitude: </strong>${feature.properties.mag}`);
+        layer.bindPopup(`<strong>Place: </strong> ${feature.properties.place}<br><strong>Time: </strong>${new Date(feature.properties.time)}<br><strong>Magnitude: </strong>${feature.properties.mag}`);
     };
 
     // Set up the legend
@@ -69,8 +69,7 @@ function createMap(response) {
 
         div.innerHTML = legendInfo;
 
-        // go through each magnitude item to label and color the legend
-        // push to labels array as list item
+        // loop through depth to label and color the legend
         for (var i = 0; i < depths.length; i++) {
             labels.push('<li style="background-color:' + circleColor(depths[i] + 1) + '"> <span>' + depths[i] + (depths[i + 1] ? '&ndash;' + depths[i + 1] + '' : '+') + '</span></li>');
         }
@@ -81,16 +80,16 @@ function createMap(response) {
         return div;
     };
 
-    // Adding legend to the map
+    // Add to map
     legend.addTo(myMap);
 
-}; // end createMap function
+}; 
 
-// Define a markerSize function that will give each city a different marker radius based on earthquake magnitude
+// Function to base radius size on magnitude
 function markerSize(magnitude) {
     return magnitude * 10;
 }
-//Function to base color on the depth of Earthquake
+//Function to change circle color depending on depth
 function circleColor(depth) {
     if (depth <5) {
         return "pink"
@@ -112,7 +111,7 @@ function circleColor(depth) {
     }
 };
 
-// Perform an API call to the USGS earthquakes API to get earthquake information. 
+// API call  
 d3.json(queryUrl, function(response) {
 
     // Call createMap with response.features
